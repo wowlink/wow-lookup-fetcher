044b3a65269b6d1b0370e585167797bf3c14b86d
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-11 09:07:20 +0000

    refactor: introduce config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import { WowLookupFetcher, WowLookupFetchRequest, WowLookupFetchResponse } from "wow-interface";
+import { WowLookupFetcher, WowLookupFetcherConfig, WowLookupFetchRequest, WowLookupFetchResponse } from "wow-interface";
 
 class GitHubWowLookupFetcher implements WowLookupFetcher {
+    config_: WowLookupFetcherConfig;
+
+    constructor(config: WowLookupFetcherConfig) {
+        this.config_ = config;
+    }
+
     fetch(req: WowLookupFetchRequest): WowLookupFetchResponse {
         throw new Error("Method not implemented.");
     }
