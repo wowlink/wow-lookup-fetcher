6181148e32ca612239b66b080cb38661a575b6ca
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-11 08:51:21 +0000

    feat: add fake API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-class GitHubWowLookupFetcher {}
+import { WowLookupFetcher, WowLookupFetchRequest, WowLookupFetchResponse } from "wow-interface";
+
+class GitHubWowLookupFetcher implements WowLookupFetcher {
+    fetch(req: WowLookupFetchRequest): WowLookupFetchResponse {
+        throw new Error("Method not implemented.");
+    }
+}
